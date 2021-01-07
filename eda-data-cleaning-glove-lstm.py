129ed7af975f1d120e3f466106ee7fd3969f3c90
Prakhar Bansal <prakharb13@gmail.com>
2021-01-07 22:00:09 +0530

    Update eda-data-cleaning-glove-lstm.py

--- eda-data-cleaning-glove-lstm.py
+++ eda-data-cleaning-glove-lstm.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -18,33 +12,17 @@ import collections
 from collections import Counter
 import wordcloud
 from wordcloud import WordCloud
-
-
-# In[2]:
-
-
 import os
 for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
 
-# In[3]:
-
-
 pd.set_option('display.max_colwidth',200)
-
-
-# In[4]:
-
-
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s',datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger(__name__)
 logger.info('Logger initialised...')
-
-
-# In[5]:
 
 
 train_df=pd.read_csv('/kaggle/input/nlp-getting-started/train.csv')
@@ -52,59 +30,26 @@ train_df=pd.read_csv('/kaggle/input/nlp-getting-started/train.csv')
 
 # ## EDA
 
-# In[6]:
-
-
-train_df.shape
-
-
-# In[7]:
-
-
-train_df['target'].value_counts()
-
-
-# In[8]:
-
+print(train_df.shape)
+print(train_df['target'].value_counts())
 
 sns.countplot(train_df['target'])
-
-
-# In[9]:
 
 
 logger.info("% of samples where keyword column is 0")
 len(train_df[train_df['keyword'].isna()])*100/len(train_df)
 
 
-# In[10]:
-
-
 logger.info("% of samples where location column is 0")
 len(train_df[train_df['location'].isna()])*100/len(train_df)
 
 
-# In[11]:
-
-
 sns.barplot(train_df['keyword'].value_counts()[:20].values,train_df['keyword'].value_counts()[:20].index,orient="H")
-
-
-# In[12]:
-
 
 ## Keyword chart when target is 1
 sns.barplot(train_df[train_df['target']==1]['keyword'].value_counts()[:20].values,train_df[train_df['target']==1]['keyword'].value_counts()[:20].index,orient="H")
 
-
-# In[13]:
-
-
 sns.barplot(train_df['location'].value_counts()[:20].values,            train_df['location'].value_counts()[:20].index,orient="H")
-
-
-# In[14]:
-
 
 ## Location chart when target is 1
 sns.barplot(train_df[train_df['target']==1]['location'].value_counts()[:20].values,            train_df[train_df['target']==1]['location'].value_counts()[:20].index,orient="H")
@@ -123,25 +68,14 @@ sns.barplot(train_df[train_df['target']==1]['location'].value_counts()[:20].valu
 
 # #### 1. Make text lowercase
 
-# In[15]:
-
-
 def to_lowercases(x):
     return x.lower()
 
 train_df['text']=train_df['text'].apply(to_lowercases)
-
-
-# In[16]:
-
-
-train_df.head(1)
+print(train_df.head(1))
 
 
 # #### Clean text
-
-# In[17]:
-
 
 def clean_text(x):
     text = re.sub('(\d+)','',x)    
@@ -154,9 +88,6 @@ train_df.head(2)
 
 # #### Remove URL
 
-# In[18]:
-
-
 def remove_url(x):
     text = re.sub('(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})\/([a-zA-Z0-9_]+]*)',' ',x)
     return text
@@ -166,7 +97,7 @@ train_df['text'] = train_df['text'].apply(remove_url)
 
 # #### Remove Punctuations
 
-# In[19]:
+
 
 
 def remove_punct(x):
@@ -179,7 +110,7 @@ train_df['text'] = train_df['text'].apply(remove_punct)
 
 # #### Tokenization
 
-# In[20]:
+
 
 
 def get_tokens(x):
@@ -191,7 +122,7 @@ train_df['text'] = train_df['text'].apply(get_tokens)
 
 # #### Remove Stop Words
 
-# In[21]:
+
 
 
 stop_words = nltk.corpus.stopwords.words('english')
@@ -206,7 +137,7 @@ train_df['text'] = train_df['text'].apply(remove_stop_words)
 
 # #### Lemmatization
 
-# In[22]:
+
 
 
 from nltk.stem import WordNetLemmatizer
@@ -221,59 +152,32 @@ def lemmatization(x):
     
 train_df['text_lemm'] = train_df['text'].apply(lemmatization)
 
-
-# In[23]:
-
-
-train_df.head(2)
+print(train_df.head(2))
 
 
 # ### Text Analysis
-
-# In[24]:
-
-
 from nltk import FreqDist
-
-
-# In[25]:
-
-
 fdist = FreqDist()
 def freq_dist(x):
     for word in x:
         fdist[word]+=1
     
     return fdist
-
-
-# In[26]:
-
-
 train_df['text_lemm'].apply(freq_dist)[1]
 
 
-# In[27]:
-
-
 fdist = FreqDist()
 def freq_dist(x):
     for word in x:
         fdist[word]+=1
     
     return fdist
-
-
-# In[28]:
 
 
 most_common = Counter(train_df['text_lemm'].apply(freq_dist)[1]).most_common(50)
 l=[]
 for k,v in most_common:
     l.append(k.replace("\'",''))
-
-
-# In[29]:
 
 
 wordcloud = WordCloud(background_color='white',
@@ -287,7 +191,6 @@ plt.imshow(wordcloud)
 
 # #### Bigrams and Trigrams
 
-# In[30]:
 
 
 fdist = nltk.FreqDist()
@@ -299,21 +202,8 @@ def bigrams(x):
     return fdist
 
 
-# In[31]:
-
-
 bigrams = train_df['text_lemm'].apply(bigrams)
-
-
-# In[32]:
-
-
 Counter(fdist).most_common(20)
-
-
-# In[33]:
-
-
 fdist = nltk.FreqDist()
 def trigrams(x):
     y = list(nltk.trigrams(x))
@@ -322,31 +212,15 @@ def trigrams(x):
     
     return fdist
 
-
-# In[34]:
-
-
 trigrams = train_df['text_lemm'].apply(trigrams)
-
-
-# In[35]:
-
-
 Counter(fdist).most_common(20)
 
 
 # #### Bag of Words
 
-# In[36]:
-
-
 l = []
 for i in range(len(train_df)):
     l.append(' '.join(train_df.loc[i,'text_lemm']))
-
-
-# In[37]:
-
 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -358,10 +232,6 @@ countvect_text.get_shape()
 
 
 # #### TFIDF
-
-# In[38]:
-
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 tf = TfidfVectorizer()
@@ -370,10 +240,6 @@ tf_text.shape
 
 
 # #### Glove Embeddings
-
-# In[39]:
-
-
 f = open('../input/glove-100d-word-embeddings/glove.6B.100d.txt')
 embeddings = {}
 for line in f:
@@ -382,46 +248,22 @@ for line in f:
 f.close()
 
 
-# In[40]:
-
-
 labels = train_df.target.values
 
-
-# In[41]:
 
 
 from keras.preprocessing.sequence import pad_sequences
 from keras.preprocessing.text import Tokenizer
 from keras.utils import to_categorical,plot_model
 
-
-# In[42]:
-
-
 tokenizer = Tokenizer(num_words=10000)
-
 tokenizer.fit_on_texts(l)
-
 sequences = tokenizer.texts_to_sequences(l)
-
 word_index = tokenizer.word_index
 
 
-# In[43]:
-
-
 print("Unique Tokens %s"%len(tokenizer.word_index))
-
-
-# In[44]:
-
-
 data = pad_sequences(list(sequences),maxlen=20,truncating='post',padding='post')
-
-
-# In[45]:
-
 
 indices = np.arange(data.shape[0])
 np.random.shuffle(indices)
@@ -432,24 +274,13 @@ labels_y = labels[indices]
 nb_validation_sample = int(.15*data.shape[0])
 
 
-# In[46]:
-
 
 x_train = data[:-nb_validation_sample]
 y_train = labels_y[:-nb_validation_sample].reshape(-1,1)
 x_test = data[-nb_validation_sample:]
 y_test = labels_y[-nb_validation_sample:].reshape(-1,1)
 
-
-# In[47]:
-
-
 embedding_matrix = np.zeros((len(word_index) + 1, 100))
-
-
-# In[48]:
-
-
 for word,i in word_index.items():
     try:
         vector = embeddings[word]
@@ -460,9 +291,6 @@ for word,i in word_index.items():
         continue
 
 
-# In[49]:
-
-
 from tensorflow.keras import backend
 
 from tensorflow.keras.layers import Input,Dense,Activation,Embedding,Flatten,LSTM,Dropout,SpatialDropout1D
@@ -470,8 +298,6 @@ from tensorflow.keras.models import Model,Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint
 
-
-# In[50]:
 
 
 inp = Input(shape=(x_train.shape[1],))
@@ -492,29 +318,13 @@ adam = Adam(learning_rate=.001)
 model = Model(inputs=[inp],outputs=[out])
 
 
-# In[51]:
-
-
 model.compile(loss='binary_crossentropy',optimizer=adam,metrics=['accuracy'])
-
-
-# In[52]:
-
-
 model.fit(x_train,y_train,verbose=1,
           batch_size=4,
           epochs=10,
           validation_data=[x_test,y_test])
 
-
-# In[53]:
-
-
 model.summary()
-
-
-# In[ ]:
-
 
 
 
